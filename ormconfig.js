ebfb5d38c804f12d486a261d9dddda85e3b2f47b
Cleiton Lok <cleiton.biou@gmail.com>
2021-03-25 22:40:32 -0300

    fix
    Signed-off-by: Cleiton Lok <cleiton.biou@gmail.com>

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,11 @@
-console.log("process.env.DATABASE_URL :>>", process.env.DATABASE_URL)
-
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "ssl": {
         "rejectUnauthorized": false
     },
-    "migrations": ["./dist/database/migrations/*.js"],
-    "entities": ["./dist/models/*.js"],
+    "migrations": ["dist/database/migrations/*.js"],
+    "entities": ["dist/models/*.js"],
     "logging": false,
     "cli": {
         "migrationsDir": "./src/database/migrations"
